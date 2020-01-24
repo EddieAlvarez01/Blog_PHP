52da9252b075c11aa9f5e2e192fec9cd48f56afa
Eddie Alvarez <eddieaugusto040@gmail.com>
2020-01-24 13:58:26 -0600

    EStilos css de la pagina

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="es">
 	<head>
 		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
 		<title>Blog de Videojuegos</title>
 	</head>
 	<body>
@@ -30,6 +31,7 @@
 					</li>
 				</ul>
 			</nav>
+			<div class="clearfix"></div>
 		</header>
 		
 		<div id="container">
@@ -62,19 +64,24 @@
 			</aside>
 			
 		    <!-- CONTENIDO PRINCIPAL -->
-		    <div class="main">
+		    <div id="main">
 		    	<h1>Últimas entradas</h1>
 		    	<article class="entry">
-		    		<h2>Título de mi entrada</h2>
-		    		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-		    		 standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
-		    		  specimen book.
-		    		 It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-		    		 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-		    		  with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		    		<a href="">
+		    			<h2>Título de mi entrada</h2>
+    		    		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+    		    		 standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
+    		    		  specimen book.
+    		    		 It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+    		    		 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+    		    		  with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		    		</a>
 		    	</article>
+		    	<div id="seeall">
+		    		<a href="#">Ver todas las entradas</a>
+		    	</div>
 		    </div>
-		    
+		    <div class="clearfix"></div>
 		</div>
 		
 		<!-- PIE DE PAGINA -->
